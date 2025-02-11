4331ed5370124d1546711b84e3476a20f1b13c66
Lorenzo Massone <lorenzomassone@gmail.com>
2025-02-11 12:06:28 +0100

    chore: increased dataset size

--- build_checker/res/akka_placeholder/src/main/scala/Main.scala
+++ build_checker/res/akka_placeholder/src/main/scala/Main.scala
@@ -1,51 +1,30 @@
 import akka.actor.{Actor, ActorSystem, Props}
+import akka.routing.RoundRobinPool
 
-case object Toggle
-case object Reset
-case class Msg(content: String)
+case class Work(job: String)
 
-class ToggleActor extends Actor {
-  // Start in active state
-  def receive: Receive = active
-
-  def active: Receive = {
-    case Toggle =>
-      println("ToggleActor: Switching from active to inactive.")
-      context.become(inactive)
-    case Reset =>
-      println("ToggleActor: Already active, staying active.")
-      // Remain in the same active state
-    case Msg(content) =>
-      println(s"ToggleActor (active) got -> $content")
+class WorkerActor extends Actor {
+  def receive: Receive = {
+    case Work(job) =>
+      println(s"${self.path.name} working on: $job")
     case _ =>
-      println("ToggleActor (active) received unknown message.")
-  }
-
-  def inactive: Receive = {
-    case Toggle =>
-      println("ToggleActor: Switching from inactive to active.")
-      context.become(active)
-    case Reset =>
-      println("ToggleActor: Reset invoked, going to active.")
-      context.become(active)
-    case Msg(content) =>
-      println(s"ToggleActor (inactive) got -> $content")
-    case _ =>
-      println("ToggleActor (inactive) received unknown message.")
+      println(s"${self.path.name} received an unknown message.")
   }
 }
 
-object ToggleApp extends App {
-  val system = ActorSystem("ToggleSystem")
-  val toggler = system.actorOf(Props[ToggleActor](), "toggleActor")
+object RouterActorApp extends App {
+  val system = ActorSystem("RouterSystem")
 
-  toggler ! Msg("Hello in active mode.")
-  toggler ! Toggle
-  toggler ! Msg("Now I'm in inactive mode.")
+  // Create a router with 3 workers
+  val router = system.actorOf(
+    RoundRobinPool(3).props(Props[WorkerActor]()),
+    "workerRouter"
+  )
 
-  // Use Reset to go back to active no matter what
-  toggler ! Reset
-  toggler ! Msg("Back in active mode.")
+  // Send multiple tasks
+  for (i <- 1 to 5) {
+    router ! Work(s"Task-$i")
+  }
 
   Thread.sleep(500)
   system.terminate()
