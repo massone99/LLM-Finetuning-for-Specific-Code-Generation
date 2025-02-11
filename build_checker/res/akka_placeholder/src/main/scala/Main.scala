4bdd2e8208545989f71fc670f290461c1855c4f0
Lorenzo Massone <lorenzomassone@gmail.com>
2025-02-11 16:44:42 +0100

    chore: increased dataset size

--- build_checker/res/akka_placeholder/src/main/scala/Main.scala
+++ build_checker/res/akka_placeholder/src/main/scala/Main.scala
@@ -1,49 +1,43 @@
-// TypedStashExample.scala
+// StdinStreamTyped.scala
+// This example reads lines from standard input using a stream and prints each line.
 
-import akka.actor.typed.scaladsl.{Behaviors, StashBuffer}
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorSystem, Behavior}
+import akka.stream.scaladsl.{Framing, Sink, StreamConverters}
+import akka.util.ByteString
+import akka.stream.SystemMaterializer
+import scala.concurrent.ExecutionContext
+import akka.{Done, NotUsed}
+import scala.concurrent.Future
 
-object StashActor {
-  sealed trait Command
-  case object Ready extends Command
-  case class Payload(msg: String) extends Command
+object StdinStreamApp extends App {
+  // Create a simple root behavior that sets up the stream
+  val root: Behavior[Nothing] = Behaviors.setup[Nothing] { context =>
+    implicit val mat = SystemMaterializer(context.system).materializer
+    implicit val ec: ExecutionContext = context.executionContext
 
-  def apply(): Behavior[Command] = Behaviors.withStash(10) { buffer =>
-    Behaviors.setup { context =>
+    // Create a source from System.in
+    val source: Source[String, NotUsed] = StreamConverters.fromInputStream(() => System.in)
+      .via(
+        Framing.delimiter(
+          ByteString("\n"),         // Use ByteString for the newline delimiter
+          maximumFrameLength = 256,
+          allowTruncation = true
+        )
+      )
+      .map(_.utf8String)             // Convert bytes to UTF-8 string
 
-      def unstashedBehavior: Behavior[Command] = Behaviors.receiveMessage {
-        case Payload(m) =>
-          context.log.info(s"Processing payload: $m")
-          Behaviors.same
-        case Ready =>
-          context.log.info("Already in ready state.")
-          Behaviors.same
-      }
+    // Sink that prints each line
+    val sink: Sink[String, Future[Done]] = Sink.foreach(line => println(s"You typed: $line"))
 
-      // Initially not ready
-      val initial: Behavior[Command] = Behaviors.receiveMessage {
-        case Ready =>
-          context.log.info("Becoming ready, unstashing messages.")
-          buffer.unstashAll(unstashedBehavior)
-        case other =>
-          context.log.info("Stashing message until we get Ready.")
-          buffer.stash(other)
-          Behaviors.same
-      }
-
-      initial
+    // Run the stream
+    val done: Future[Done] = source.runWith(sink)
+    done.onComplete { _ =>
+      context.system.terminate()
     }
+
+    Behaviors.empty
   }
-}
 
-object TypedStashExampleApp extends App {
-  val system = ActorSystem(StashActor(), "StashSystem")
-
-  system ! StashActor.Payload("msg1")
-  system ! StashActor.Payload("msg2")
-  system ! StashActor.Ready
-  system ! StashActor.Payload("msg3")
-
-  Thread.sleep(500)
-  system.terminate()
+  val system = ActorSystem(root, "StdinStreamSystem")
 }
